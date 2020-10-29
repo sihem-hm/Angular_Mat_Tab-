ec851fb6da0f09b3d57c36b3828a8ff1c1dee4a1
sihem-hm <hammamisihem6@gmail.com>
2020-10-29 12:41:43 +0100

    drop-dow-fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,153 +4,135 @@ import { SelectItem } from 'primeng/api';
 import { MatTable } from '@angular/material/table';
 import { MatDialog } from '@angular/material/dialog';
 import { TableAddComponent } from './table-add/table-add.component';
- import { MessageService } from 'primeng/api';
-import {Table , TableService } from 'primeng/table';
-import {FormControl} from '@angular/forms';
-import { Product } from './product';
+import { MessageService } from 'primeng/api';
+import { Table, TableService } from 'primeng/table';
+import { FormControl } from '@angular/forms';
 import { ProductService } from './productservice';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
-import { FormGroup,FormBuilder, Validators, AbstractControl, ValidatorFn, FormArray } from '@angular/forms';
+
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn, FormArray } from '@angular/forms';
 
 export interface UsersData {
   name: string;
   job: string;
   id: number;
-  timeOfDay : Date;
+  timeOfDay: Date;
   description: string;
   action: string;
 
 }
-/* export interface Food {
-  value: string;
-  viewValue: string;
-}
-  const foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
-  ]; */
-
-
-
 
 const ELEMENT_DATA: UsersData[] = [
-  {id: 1560608769632, name: 'Artificial Intelligence', job:' teacher',timeOfDay : new Date('2020-04-24'),description :"it will be shown in expended row ",action:"delete "},
-  {id: 1560608796014, name: 'Machine Learning', job:'teacher',timeOfDay : new Date('2020-04-24'),description :"it will be shown in expended row",action:"add "},
-  {id: 1560608787815, name: 'Robotic Process Automation' , job: 'teacher',timeOfDay : new Date('2020-04-24'),description :"it will be shown in expended row",action:"delete "},
-  {id: 1560608805101, name: 'Blockchain' , job: 'teacher',timeOfDay : new Date('2020-04-24'),description :"it will be shown in expended row",action:"add "}
+  { id: 1560608769632, name: 'Artificial Intelligence', job: ' teacher', timeOfDay: new Date('2020-04-24'), description: "it will be shown in expended row ", action: "delete " },
+  { id: 1560608796014, name: 'Machine Learning', job: 'teacher', timeOfDay: new Date('2020-04-24'), description: "it will be shown in expended row", action: "add " },
+  { id: 1560608787815, name: 'Robotic Process Automation', job: 'teacher', timeOfDay: new Date('2020-04-24'), description: "it will be shown in expended row", action: "delete " },
+  { id: 1560608805101, name: 'Blockchain', job: 'teacher', timeOfDay: new Date('2020-04-24'), description: "it will be shown in expended row", action: "add " }
 ];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ TableService, {
-    provide: Table}],
-    
-    animations: [
-      trigger('detailExpand', [
-        state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
-        state('expanded', style({height: '*'})),
-        transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-      ]),
-    ],
+  providers: [TableService, {
+    provide: Table
+  }],
+
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0', display: 'none' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class AppComponent {
-  icon : any;
-  displayedColumns: string[] = ['icon','id', 'name', 'job', 'timeOfDay','statuses']
+  icon: any;
+  displayedColumns: string[] = ['icon', 'id', 'name', 'job', 'timeOfDay', 'statuses']
+
 
   dataSource = ELEMENT_DATA;
   customerForm: FormGroup;
   toppings = new FormControl();
 
-  products1: Product[];
-  isAvailable : any;
-    products2: Product[];
-    expandedElement : string;
-    
-    statuses: SelectItem[];
 
-    clonedProducts: { [s: string]: Product; } = {};
-  
+  isAvailable: any;
+
+  expandedElement: string;
+
+  statuses: SelectItem[];
+
+
+
   toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
-  @ViewChild(MatTable,{static:true}) table: MatTable<any>;
+  @ViewChild(MatTable, { static: true }) table: MatTable<any>;
 
-  constructor(public dialog: MatDialog,private productService: ProductService ,
-    
-    
-    private messageService: MessageService) {}
+  constructor(public dialog: MatDialog,
+
+
+    private messageService: MessageService) { }
   ngOnInit() {
-  
-this.expandedElement = "aa";
-
-    this.statuses = [{label: 'In Stock', value: 'INSTOCK'},{label: 'Low Stock', value: 'LOWSTOCK'},{label: 'Out of Stock', value: 'OUTOFSTOCK'}]
-}
-nativeSelectFormControl = new FormControl('valid', [
-  Validators.required,
-  Validators.pattern('valid'),
-]);
-
-save(): void {
-  console.log(this.customerForm);
-  console.log('Saved: ' + JSON.stringify(this.customerForm.value));
-}
 
 
-openDialog(action,obj) {
+    this.expandedElement = "aa";
+
+    this.statuses = [{ label: 'In Stock', value: 'INSTOCK' }, { label: 'Low Stock', value: 'LOWSTOCK' }, { label: 'Out of Stock', value: 'OUTOFSTOCK' }]
+  }
+  nativeSelectFormControl = new FormControl('valid', [
+    Validators.required,
+    Validators.pattern('valid'),
+  ]);
+
+  save(): void {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+
+  openDialog(action, obj) {
     obj.action = action;
     const dialogRef = this.dialog.open(TableAddComponent, {
       width: '250px',
-      data:obj
+      data: obj
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      if(result.event == 'Add'){
+      if (result.event == 'Add') {
         this.addRowData(result.data);
-        
+
       }
-      else if(result.event == 'Delete'){
+      else if (result.event == 'Delete') {
         this.deleteRowData(result.data);
       }
     });
   }
 
-  addRowData(row_obj){
+  addRowData(row_obj) {
     var d = new Date();
     this.dataSource.push({
-      id:d.getTime(),
-      name:row_obj.name,
-      job:row_obj.job,
-      action:row_obj.action,
-      timeOfDay : row_obj.timeOfDay,
-      description  : row_obj.description,
+      id: d.getTime(),
+      name: row_obj.name,
+      job: row_obj.job,
+      action: row_obj.action,
+      timeOfDay: row_obj.timeOfDay,
+      description: row_obj.description,
     });
     this.table.renderRows();
-    
+
   }
-
-  onRowEditInit(product: Product) {
-    this.clonedProducts[product.id] = {...product};
-}
-
-
-  updateRowData(row_obj){
-    this.dataSource = this.dataSource.filter((value,key)=>{
-      if(value.id == row_obj.id){
+   updateRowData(row_obj) {
+    this.dataSource = this.dataSource.filter((value, key) => {
+      if (value.id == row_obj.id) {
         value.name = row_obj.name;
       }
       return true;
     });
   }
-  deleteRowData(row_obj){
-    this.dataSource = this.dataSource.filter((value,key)=>{
+   deleteRowData(row_obj) {
+    this.dataSource = this.dataSource.filter((value, key) => {
       return value.id != row_obj.id;
     });
   }
-  onRowEditCancel(product: Product, index: number) {
-    this.products2[index] = this.clonedProducts[product.id];
-    delete this.products2[product.id];
-}
+
 }
